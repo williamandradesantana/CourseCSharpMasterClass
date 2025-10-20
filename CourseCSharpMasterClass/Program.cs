33a02afc2e0f57a0b5d1f01519da30d4fb874c62
William Santana <williamandrade1058@gmail.com>
2025-10-20 11:46:09 -0300

    Finalizing our Addition Calculator

--- CourseCSharpMasterClass/Program.cs
+++ CourseCSharpMasterClass/Program.cs
@@ -3,29 +3,30 @@
 using System;
 using CourseCSharpMasterClass.section1;
 
-//Console.WriteLine("First program!");
+Console.WriteLine("First program!");
 
-//// declare a variable
-//string myFriendsName;
-//// assign a value to the myFrindsName variable
-//myFriendsName = "Jannick";
+// declare a variable
+string myFriendsName;
+// assign a value to the myFrindsName variable
+myFriendsName = "Jannick";
 
-//// use/access a variable
-//Console.WriteLine(myFriendsName);
+// use/access a variable
+Console.WriteLine(myFriendsName);
 
-//// declare a variable and assign a value to it.
-//string myFriendsName2 = "Frank";
-//Console.WriteLine(myFriendsName2);
+// declare a variable and assign a value to it.
+string myFriendsName2 = "Frank";
+Console.WriteLine(myFriendsName2);
 
-///*
-//    overwritting the variable value
-//    myFriendsName = "Frank";
-//    Console.WriteLine(myFriendsName);
-//*/
+/*
+    overwritting the variable value
+    myFriendsName = "Frank";
+    Console.WriteLine(myFriendsName);
+*/
 
-//Console.WriteLine("Hello, William!");
+Console.WriteLine("Hello, William!");
 
-//UnderstandingUserInputAndTheDataTypeString.Run();
-//OverviewOfDataType.Run();
-//VariableDefinitionInitializationAndAssigning.Run();
+UnderstandingUserInputAndTheDataTypeString.Run();
+OverviewOfDataType.Run();
+VariableDefinitionInitializationAndAssigning.Run();
 UnderstandingIntNumberDatatypeAndWhyWeCantFitAStringIntoIt.Run();
+FinalizingOurAdditionCalculator.Run();
---
+++ CourseCSharpMasterClass/section1/FinalizingOurAdditionCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace CourseCSharpMasterClass.section1;
+
+public class FinalizingOurAdditionCalculator
+{
+    public static void Run()
+    {
+        int firstNumber = 0;
+        int secondNumber = 0;
+
+        Console.WriteLine("Enter a whole number!");
+
+        string userInput = Console.ReadLine()!;
+        firstNumber = int.Parse(userInput);
+
+        Console.WriteLine("Enter a whole number!");
+        userInput = Console.ReadLine()!;
+        secondNumber = int.Parse(userInput);
+
+        int sum = firstNumber + secondNumber;
+
+        Console.WriteLine($"The result of {firstNumber} + {secondNumber} is {sum}");
+        Console.ReadKey();
+    }
+}
